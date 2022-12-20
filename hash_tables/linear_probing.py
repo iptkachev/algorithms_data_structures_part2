0d5b4ea01165c2d90d55fa76449c07177758d807
Ilya Tkachev <skaatt@mail.ru>
2022-12-20 22:31:28 +0100

    add new func for hash tables

--- hash_tables/linear_probing.py
+++ hash_tables/linear_probing.py
@@ -1,17 +1,9 @@
 class HashTable:
+	_DELETED = ""
+
 	def __init__(self, size_m: int):
 		self._size_m = size_m
 		self._hash_table = [None for _ in range(self._size_m)]
-
-	def get_index_by_key(self, k: int) -> int | None:
-		index = self._get_hash(k)
-		while True:
-			if self._hash_table[index] == k:
-				return index
-			index = (index + 1) % self._size_m
-
-			if index == self._get_hash(k):
-				return None
 
 	def insert(self, k: int) -> bool:
 		index = self._get_hash(k)
@@ -19,7 +11,7 @@ class HashTable:
 		while True:
 			if self._hash_table[index] == k:
 				return False
-			elif self._hash_table[index] is None:
+			elif self._hash_table[index] in (None, self._DELETED):
 				self._hash_table[index] = k
 				return True
 			else:
@@ -29,6 +21,28 @@ class HashTable:
 				self._rehash()
 				self.insert(k)
 				return True
+
+	def get_index_by_key(self, k: int) -> int | None:
+		index = self._get_hash(k)
+
+		while True:
+			if self._hash_table[index] == k:
+				return index
+			elif self._hash_table[index] is None:
+				return None
+			else:
+				index = (index + 1) % self._size_m
+
+			if index == self._get_hash(k):
+				return None
+
+	def delete(self, k: int) -> bool:
+		index = self.get_index_by_key(k)
+		if index is None:
+			return False
+		else:
+			self._hash_table[index] = self._DELETED
+			return True
 
 	def _get_hash(self, k: int) -> int:
 		return k % self._size_m
@@ -43,7 +57,15 @@ class HashTable:
 
 if __name__ == "__main__":
 	hash_table = HashTable(5)
-	for k in [ord(char) for char in "RHCD"]:
+	for k in [ord(char) for char in "HCDR"]:
+		print(chr(k), k, k % 5)
 		hash_table.insert(k)
 		print(hash_table._hash_table)
 		print(hash_table.get_index_by_key(k))
+	hash_table.delete(67)
+	print(hash_table._hash_table)
+	print(hash_table.get_index_by_key(68) is not None)
+	for k in hash_table._hash_table:
+		if isinstance(k, int):
+			print(chr(k), end=" ")
+
